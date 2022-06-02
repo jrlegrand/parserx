65bc5b2135d58aff0e2961d1a1f7a424c7ac367d
Joey LeGrand <jrlegrand@gmail.com>
2022-06-02 12:36:52 +0000

    Continue optimizing

--- parsers/when.py
+++ parsers/when.py
@@ -6,7 +6,7 @@ from .classes.parser import *
 class WhenParser(Parser):
 	parser_type = 'when'
 	match_keys = ['when', 'when_text_start', 'when_text_end', 'when_text']
-	pattern = r'(?P<when_relation>with|\bc\.|\bc|before|\ba|\ba\.|after|\bp|\bp\.|in the|at|every|\bq|\bq.|night)(?: each| every)?(?P<when_time>\s?(?:c\b|c\.\b|meal(?:s)? and at bedtime|meal(?:s)?|c\.m\.\b|cm\b|breakfast|c\.d\.\b|cd\b|lunch|c\.v\.\b|cv\b|dinner|morning|morn|\ba\.m\.\b|\bam\b|evening at bedtime|bedtime|evening|eve|aft(?:ernoon)?|\bp\.m\.\b|\bpm\b|night|hs\b|h\.s\.\b|ly))'
+	pattern = r'(?P<when_relation>with|\bc\.|\bc|before|\ba|\ba\.|after|\bp|\bp\.|in the|at|every|each|\bq|\bq.|night)(?: each| every)?(?P<when_time>\s?(?:c\b|c\.\b|meal(?:s)? and at bedtime|meal(?:s)?|c\.m\.\b|cm\b|breakfast|c\.d\.\b|cd\b|lunch|c\.v\.\b|cv\b|dinner|morning|morn|a\.m\.\b|am\b|evening at bedtime|bedtime|evening|eve|aft(?:ernoon)?|p\.m\.\b|pm\b|night|hs\b|h\.s\.\b|ly))'
 	def normalize_match(self, match):
 		# TODO: normalize before to 'a' and after to 'p', etc
 		# TODO: normalize meals to 'm', etc
@@ -17,6 +17,17 @@ class WhenParser(Parser):
 		when_text = match.group(0)
 		return self.generate_match({'when': when, 'when_text_start': when_text_start, 'when_text_end': when_text_end, 'when_text': when_text})
 
+# Things that don't match the more common patterns above
+class OtherWhenParser(WhenParser):
+	pattern = r'(?P<when>bedtime|\bam\b|\bhs\b)'
+	def normalize_match(self, match):
+		when = match.group('when')
+		when = get_normalized(WHEN, when)
+		when_text_start, when_text_end = match.span()
+		when_text = match.group(0)
+		return self.generate_match({'when': when, 'when_text_start': when_text_start, 'when_text_end': when_text_end, 'when_text': when_text})
+
+
 """
 PC	http://hl7.org/fhir/v3/TimingEvent	PC	after meal (from lat. post cibus)
 PCM	http://hl7.org/fhir/v3/TimingEvent	PCM	after breakfast (from lat. post cibus matutinus)
@@ -25,5 +36,6 @@ PCV	http://hl7.org/fhir/v3/TimingEvent	PCV	after dinner (from lat. post cibus ve
 """
 
 parsers = [
-	WhenParser()
+	WhenParser(),
+	OtherWhenParser(),
 ]
