8e708af50be99e5acf196b6c25dc4a68cd2c2f40
Joey LeGrand <jrlegrand@gmail.com>
2022-07-14 11:45:24 +0000

    For up to x days
    Fixes #21

--- parsers/duration.py
+++ parsers/duration.py
@@ -20,7 +20,7 @@ class DurationParser(Parser):
 
 # for x [more] days
 class DurationParserForXDays(DurationParser):
-    pattern = r'(?:for|x)\s*(?P<duration>' + RE_RANGE + r')\s*(?:more)?\s*(?P<duration_unit>year|month|week|day|yr\b|mon\b|wk\b|d\b)'
+    pattern = r'(?:for|x)\s*(?P<duration>' + RE_RANGE + r')\s*(?:more)?\s?(?P<duration_unit>year|month|week|day|yr\b|mon\b|wk\b|d\b)'
     def normalize_match(self, match):
         duration_range = split_range(match.group('duration'))
         duration_text_start, duration_text_end = match.span()
@@ -31,9 +31,36 @@ class DurationParserForXDays(DurationParser):
         duration_readable = self.get_readable(duration, duration_max, duration_unit)
         return self.generate_match({'duration': duration, 'duration_max': duration_max, 'duration_unit': duration_unit, 'duration_text_start': duration_text_start, 'duration_text_end': duration_text_end, 'duration_text': duration_text, 'duration_readable': duration_readable})
 
+# up to x days
+class DurationParserUpToXDays(DurationParser):
+    pattern = r'(?:for )?up to (?P<duration>' + RE_RANGE + r')\s?(?P<duration_unit>year|month|week|day|yr\b|mon\b|wk\b|d\b)'
+    def normalize_match(self, match):
+        duration_range = split_range(match.group('duration'))
+        duration_text_start, duration_text_end = match.span()
+        # NOTE: PERIOD_UNIT has the same content that duration_unit needs for normalization
+        duration_unit = get_normalized(PERIOD_UNIT, match.group('duration_unit'))
+        duration, duration_max = duration_range
+        duration_text = ' '.join(match.groups())
+        duration_readable = self.get_readable(duration, duration_max, duration_unit)
+        return self.generate_match({'duration': duration, 'duration_max': duration_max, 'duration_unit': duration_unit, 'duration_text_start': duration_text_start, 'duration_text_end': duration_text_end, 'duration_text': duration_text, 'duration_readable': duration_readable})
+    def get_readable(self, duration=None, duration_max=None, duration_unit=None):
+        plural = (duration and duration > 1) or (duration_max and duration_max > 1)
+        if duration_unit:
+            duration_unit += 's' if plural else ''
+        else:
+            duration_unit = ''
+
+        duration = str(duration) if duration else ''
+        if duration_max:
+            duration += '-' + str(duration_max)
+        
+        readable = 'for up to ' + duration + ' ' + duration_unit
+        readable = readable.strip()
+        return readable
+
 # on day x
 class DurationParserOnDayX(DurationParser):
-    pattern = r'on day(?:s)?\s*(?P<duration>' + RE_RANGE + r')'
+    pattern = r'on day(?:s)?\s?(?P<duration>' + RE_RANGE + r')'
     def normalize_match(self, match):
         duration_range = split_range(match.group('duration'))
         duration_text_start, duration_text_end = match.span()
@@ -46,6 +73,7 @@ class DurationParserOnDayX(DurationParser):
 
 parsers = [
     DurationParserForXDays(),
+    DurationParserUpToXDays(),
     DurationParserOnDayX()
 ]
 
