5da85544ca19e7ccb898d3167955111efad6d46e
Joey LeGrand <jrlegrand@gmail.com>
2022-07-18 15:32:13 +0000

    Readable function working for max_dose

--- parsers/max_dose.py
+++ parsers/max_dose.py
@@ -14,9 +14,13 @@ class MaxDoseParser(Parser):
         pattern = re.compile(r'(?:max(?: dose)|do not take more than|no more than|nmt|do not exceed)\s?(?P<dose>' + RE_RANGE + r')\s?(?P<dose_unit>' + r'|'.join(dose_patterns) + r')?\s?(?:per|/|in(?: a)?)\s?(?P<period>' + RE_RANGE + r')?\s?(?P<period_unit>day|hours|hour|hrs|hr|\bh\b|week|month|year|d\b|w\b|mon|m\b|yr)', flags = re.I)
         return pattern
     def normalize_match(self, match):
-        max_dose_numerator_value = split_range(match.group('dose'))[1] if match.group('dose') else None
+        dose_range = split_range(match.group('dose'))
+        max_dose_numerator_value = dose_range[1] or dose_range[0]
         max_dose_numerator_unit = get_normalized(DOSE_UNITS, match.group('dose_unit')) if match.group('dose_unit') else None
-        max_dose_denominator_value = split_range(match.group('period'))[1] if match.group('period') else None
+        max_dose_denominator_value = None
+        if match.group('period'):
+            period_range = split_range(match.group('period')) 
+            max_dose_denominator_value = period_range[1] or period_range[0]
         max_dose_denominator_unit = get_normalized(PERIOD_UNIT, match.group('period_unit')) if match.group('period_unit') else None
         max_dose_text_start, max_dose_text_end = match.span()
         max_dose_text = match[0]
@@ -28,11 +32,20 @@ class MaxDoseParser(Parser):
         return self.generate_match({'max_dose_numerator_value': max_dose_numerator_value, 'max_dose_numerator_unit': max_dose_numerator_unit, 'max_dose_denominator_value': max_dose_denominator_value, 'max_dose_denominator_unit': max_dose_denominator_unit, 'max_dose_text_start': max_dose_text_start, 'max_dose_text_end': max_dose_text_end, 'max_dose_text': max_dose_text, 'max_dose_readable': max_dose_readable})
     def get_readable(self, max_dose_numerator_value=None, max_dose_numerator_unit=None, max_dose_denominator_value=None, max_dose_denominator_unit=None):
         readable = 'max dose'
-        readable += ' ' + max_dose_numerator_value if max_dose_numerator_value else ''
-        readable += ' ' + max_dose_numerator_unit if max_dose_numerator_unit else ''
+        readable += ' ' + str(max_dose_numerator_value) if max_dose_numerator_value else ''
+        plural_dose_unit = max_dose_numerator_value and max_dose_numerator_value > 1
+        if max_dose_numerator_unit:
+            if plural_dose_unit:
+                max_dose_numerator_unit += 'e' if max_dose_numerator_unit[-1:] == 'h' else ''
+                max_dose_numerator_unit += 's' if max_dose_numerator_unit not in ['oz','mL','L','cm'] else ''
+            readable += ' ' + max_dose_numerator_unit
+
         readable += ' per' if max_dose_denominator_value or max_dose_denominator_unit else ''
-        readable += ' ' + max_dose_denominator_value if max_dose_denominator_value else ''
-        readable += ' ' + max_dose_denominator_unit if max_dose_denominator_unit else ''
+        readable += ' ' + str(max_dose_denominator_value) if max_dose_denominator_value else ''
+        plural_duration = max_dose_denominator_value and max_dose_denominator_value > 1
+        if max_dose_denominator_unit:
+            max_dose_denominator_unit += 's' if plural_duration else ''
+        readable += ' ' + max_dose_denominator_unit
         return readable
 
 parsers = [
